9d7c2e87e296987eeb3cb152e487ad6c71e9740a
EzmemmedQasimov <63806411+EzmemmedQasimov@users.noreply.github.com>
2022-07-25 16:45:14 +0400

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+import { counterActions } from "../store";
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+
+  const increaseHandler = () => {
+    dispatch(counterActions.increase(3));
+  };
+
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggle());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+     {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Incrrement</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
